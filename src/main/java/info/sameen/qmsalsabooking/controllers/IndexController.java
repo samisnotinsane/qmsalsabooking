6337a51d773922f101b0bd2bdf658680282c60fc
samisnotinsane <sameen11@hotmail.com>
2018-10-19 14:29:10 +0100

    Adding index controller and page.

---
+++ src/main/java/info/sameen/qmsalsabooking/controllers/IndexController.java
@@ -0,0 +1,17 @@
+package info.sameen.qmsalsabooking.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Controller for the index page.
+ * @author sameenislam
+ * @version 1.0
+ */
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
