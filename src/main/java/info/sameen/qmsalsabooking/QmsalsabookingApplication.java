516bfbaabffd24d3f96ea702fc778f754953e7c9
samisnotinsane <sameen11@hotmail.com>
2018-10-19 13:58:45 +0100

    Initial commit.

---
+++ src/main/java/info/sameen/qmsalsabooking/QmsalsabookingApplication.java
@@ -0,0 +1,12 @@
+package info.sameen.qmsalsabooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmsalsabookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QmsalsabookingApplication.class, args);
+    }
+}
---
+++ src/test/java/info/sameen/qmsalsabooking/QmsalsabookingApplicationTests.java
@@ -0,0 +1,16 @@
+package info.sameen.qmsalsabooking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QmsalsabookingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
