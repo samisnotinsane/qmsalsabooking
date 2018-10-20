9889c50633744c30a070f4e3916cfb0fcd2ced45
samisnotinsane <sameen11@hotmail.com>
2018-10-20 21:39:09 +0100

    Adding support for updating a lesson via rest controller.

--- src/main/java/info/sameen/qmsalsabooking/controller/rest/LessonController.java
+++ src/main/java/info/sameen/qmsalsabooking/controller/rest/LessonController.java
@@ -1,10 +1,13 @@
 package info.sameen.qmsalsabooking.controller.rest;
 
+import info.sameen.qmsalsabooking.exceptions.LessonNotFoundException;
 import info.sameen.qmsalsabooking.model.Attendee;
 import info.sameen.qmsalsabooking.model.Lesson;
 import info.sameen.qmsalsabooking.service.LessonService;
 import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,30 +30,38 @@ public class LessonController {
 
     // Create
     @RequestMapping(value = "/create", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.OK)
     public void create(@RequestBody Lesson lesson) {
         lessonService.save(lesson);
     }
 
     // Retrieve
     @RequestMapping(value = "/retrieve", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
     public List<Lesson> get() {
         return  lessonService.findAll();
     }
 
     @RequestMapping(value = "/retrieve/{lessonId}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
     public Lesson get(@PathVariable long lessonId) {
         return lessonService.findById(lessonId);
     }
 
     // Update
     @RequestMapping(value = "/update/{lessonId}", method = RequestMethod.POST)
-    public Lesson update(@PathVariable String lessonId) {
-        throw new NotYetImplementedException();
+    public void update(@PathVariable long lessonId, @RequestBody Lesson lesson) throws LessonNotFoundException {
+        Lesson foundLesson = lessonService.findById(lessonId);
+        if (foundLesson == null) {
+            throw new LessonNotFoundException("Lesson with '" + lessonId + "' does not exist.");
+        }
+        lesson.setId(lessonId);
+        lessonService.save(lesson);
     }
 
     // Delete
     @RequestMapping(value = "/delete/{lessonId}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable String lessonId) {
+    public void delete(@PathVariable long lessonId) {
         lessonService.delete(lessonId);
     }
 }
